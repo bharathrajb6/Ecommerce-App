7ba101c53364c1fffb6506e59043111c70bf6ae6
bharathrajb6 <bharathrajb26@gmail.com>
2024-09-28 22:14:47 +0530

    Added order controller

---
+++ order-service/src/main/java/com/example/order_service/Controller/OrderController.java
@@ -0,0 +1,6 @@
+package com.example.order_service.Controller;
+
+public class OrderController {
+    // This is a placeholder for the OrderController class
+    // This class will be implemented in the next step
+}
