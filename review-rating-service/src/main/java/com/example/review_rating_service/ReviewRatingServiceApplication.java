0b74bff1e130639fed0e9a30531a9e50edf4a0ab
bharathrajb6 <bharathrajb26@gmail.com>
2024-11-21 15:08:36 +0530

    Added review rating service

---
+++ review-rating-service/src/main/java/com/example/review_rating_service/ReviewRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.review_rating_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewRatingServiceApplication.class, args);
+	}
+
+}
---
+++ review-rating-service/src/test/java/com/example/review_rating_service/ReviewRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.review_rating_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
