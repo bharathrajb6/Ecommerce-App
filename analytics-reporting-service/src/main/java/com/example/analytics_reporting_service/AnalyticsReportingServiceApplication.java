073fa5239657599d82360c70751fd94ce23c7333
bharathrajb6 <bharathrajb26@gmail.com>
2024-11-21 15:08:26 +0530

    Added analytics reporting service

---
+++ analytics-reporting-service/src/main/java/com/example/analytics_reporting_service/AnalyticsReportingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.analytics_reporting_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsReportingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsReportingServiceApplication.class, args);
+	}
+
+}
---
+++ analytics-reporting-service/src/test/java/com/example/analytics_reporting_service/AnalyticsReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.analytics_reporting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
